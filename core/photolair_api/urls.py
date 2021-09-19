6d8f822c5247664c99c5bf87b6314a5cfcb18b95
Harmanprit Tatla <harman.tatla@mail.utoronto.ca>
2021-09-18 23:01:41 -0500

    Update url for me view

--- core/photolair_api/urls.py
+++ core/photolair_api/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/blacklist/', BlackListTokenView.as_view(), name='blacklist_token'),
     path('users/', UserCreateView.as_view()),
-    path('users/me', MeView.as_view()),
+    path('users/me/', MeView.as_view()),
     path('users/<uuid:user_id>', UserDetailView.as_view()),
     path('images/', ImageListView.as_view()),
     path('images/<uuid:image_id>', ImageDetailView.as_view()),
